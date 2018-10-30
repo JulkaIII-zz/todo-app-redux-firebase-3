9adcb06bcc6e39b0c7815c109a38e228a8ab1ac9
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-10-30 22:59:41 +0200

    fixed firebase database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,17 @@
+// import * as firebase from "firebase";
+import firebase from "@firebase/app";
+import "@firebase/database";
+//import "firebase/firestore"; // Firestore - NoSQL DB
 
-import * as firebase from "firebase";
-
-import { FirebaseConfig } from "../config/keys";
+//import { FirebaseConfig } from "../config/keys";
+const FirebaseConfig = {
+  apiKey: "AIzaSyCFkWg6cNufaCJot_pA11T9M-JYyHj1ksQ",
+  authDomain: "todos-ec8f5.firebaseapp.com",
+  databaseURL: "https://todos-ec8f5.firebaseio.com",
+  projectId: "todos-ec8f5",
+  storageBucket: "todos-ec8f5.appspot.com",
+  messagingSenderId: "264508941802"
+};
 firebase.initializeApp(FirebaseConfig);
 
 const databaseRef = firebase.database().ref();
