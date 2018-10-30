9961929f32fe8bb43053c2060ecc7321bffa57a0
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-10-30 18:07:26 +0200

    added components and firebase

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+// import "materialize-css/dist/css/materialize.min.css";
+// import "materialize-css/dist/js/materialize.min.js";
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import "@babel/polyfill";
 import App from "./App";
 import "./styles/styles.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
