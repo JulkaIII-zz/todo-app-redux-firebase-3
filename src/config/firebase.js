d0dfa2067bcb2e5b9ad62d980bab309132e0ad5e
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-11-01 11:59:03 +0200

    added keys to gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,8 @@
 import firebase from "@firebase/app";
 import "@firebase/database";
 import "@firebase/auth";
+import { FirebaseConfig } from "./keys.js";
 
-const FirebaseConfig = {
-  apiKey: "AIzaSyCFkWg6cNufaCJot_pA11T9M-JYyHj1ksQ",
-  authDomain: "todos-ec8f5.firebaseapp.com",
-  databaseURL: "https://todos-ec8f5.firebaseio.com",
-  projectId: "todos-ec8f5",
-  storageBucket: "todos-ec8f5.appspot.com",
-  messagingSenderId: "264508941802"
-};
 const fire = firebase.initializeApp(FirebaseConfig);
 export default fire;
 
